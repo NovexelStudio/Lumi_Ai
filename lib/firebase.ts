db5571736e85b6f954c5b4339446b310d752b958
Shammas <novexelstudio@gmail.com>
2026-02-15 20:07:43 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,7 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 import { getDatabase, Database } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2EMDFXrmz-iJvEHH2pM3Jf8oLZluEWDo",
-  authDomain: "lumi-ai-afe63.firebaseapp.com",
-  databaseURL: "https://lumi-ai-afe63-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "lumi-ai-afe63",
-  storageBucket: "lumi-ai-afe63.firebasestorage.app",
-  messagingSenderId: "257238628936",
-  appId: "1:257238628936:web:e868c49db1e040e8f1b67d",
-  measurementId: "G-6Q1Q14TWB8"
+// update with config
 };
 
 let app: FirebaseApp | undefined;
